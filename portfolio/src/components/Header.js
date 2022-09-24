d305061d5c937f3d5ddd75510afddbead9b7ee65
hatbann <0urloveis_great@naver.com>
2022-09-24 21:20:05 +0900

    make mobile size

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -3,6 +3,8 @@ import { Link } from 'react-scroll';
 import logo from '../images/logo_trans.png';
 import Skills from './Skills';
 import Projects from './Projects';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 function Header({ main, about, skills, projects, address }) {
   const scrollToSection = (elelmentRef) => {
@@ -11,15 +13,24 @@ function Header({ main, about, skills, projects, address }) {
       behavior: 'smooth',
     });
   };
+  const onToggleNav = () => {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle(`${styles.show}`);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <div className={styles.logo} onClick={() => scrollToSection(main)}>
-          <img src={logo} className={styles.logo_img} />
-          <div>Hyebin Cho</div>
+        <div className={styles.header_top}>
+          <div className={styles.logo} onClick={() => scrollToSection(main)}>
+            <img src={logo} className={styles.logo_img} />
+            <div>Hyebin Cho</div>
+          </div>
+          <button className={styles.navtoggle_bar} onClick={onToggleNav}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         </div>
-        <div className={styles.menus}>
+        <div className={`${styles.nav}`} id="nav">
           <span
             onClick={() => scrollToSection(about)}
             className={styles.menu}
--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
-        <div style={{ marginBottom: '30px' }}>
+        <section style={{ marginBottom: '30px' }}>
           <h2 className={styles.skills}>Skills</h2>
           <ul>
             <li>HTML</li>
@@ -12,25 +12,25 @@ function Skills() {
             <li>JAVASCRIPT</li>
             <li>REACT</li>
           </ul>
-        </div>
-        <div>
-          <div>
-            <h2 className={styles.certification}>Certification</h2>
-            <ul>
-              <li>GTQ 그래픽기술자격 1급</li>
-              <li>정보처리기사</li>
-            </ul>
-          </div>
-        </div>
+        </section>
+        <secttion>
+          <h2 className={styles.certification}>Certification</h2>
+          <ul>
+            <li>GTQ 그래픽기술자격 1급</li>
+            <li>정보처리기사</li>
+          </ul>
+        </secttion>
       </div>
       <div className={styles.right}>
-        <h2 className={styles.tools}>Tools</h2>
-        <ul>
-          <li>Git</li>
-          <li>Figma</li>
-          <li>Photoshop</li>
-          <li>Illustrator</li>
-        </ul>
+        <section>
+          <h2 className={styles.tools}>Tools</h2>
+          <ul>
+            <li>Git</li>
+            <li>Figma</li>
+            <li>Photoshop</li>
+            <li>Illustrator</li>
+          </ul>
+        </section>
       </div>
     </div>
   );
