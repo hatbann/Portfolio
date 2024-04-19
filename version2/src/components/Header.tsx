90cc23a7721df6e07a2d382ad9647306f8c6836f
hatbann <0urloveis_great@naver.com>
2024-04-19 17:28:53 +0900

    (feat) Init portfoilo page

---
+++ version2/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <span>INTRO</span>
+        <span>SKILLS</span>
+        <span>CAREER</span>
+        <span>PROJECTS</span>
+        <span>CONTACT</span>
+      </div>
+      <div>toggle button</div>
+    </div>
+  );
+};
+
+export default Header;
