471bf485fcc138f0cde654ab2aac9da1bd83bc2b
hatbann <0urloveis_great@naver.com>
2024-05-17 17:24:43 +0900

    (feat) Add career projects

--- version2/src/components/Career.tsx
+++ version2/src/components/Career.tsx
@@ -28,22 +28,73 @@ const Career = () => {
           >
             <SwiperSlide className={style['swiper-slide']}>
               <div className={style['img-wrapper']}>
-                <img src="/images/png/rela.png" alt="rela" />
+                <img
+                  src="/images/png/rela.png"
+                  alt="rela"
+                  onClick={() => {
+                    window.open('https://rela.co.kr/');
+                  }}
+                />
               </div>
               <div className={style['contents']}>
                 <div className={style['top']}>
-                  <span>Re:La</span>
-                  <span>AI를 통해 악보를 생성하고 공유하는 서비스</span>
+                  <span
+                    className={style['top-title']}
+                    style={{ color: '#fe5000' }}
+                  >
+                    Re:La
+                  </span>
+                  <span className={style['top-info']}>
+                    AI를 통해 악보를 생성하고 공유하는 서비스
+                  </span>
                 </div>
+                <div className={style['line']}></div>
                 <div className={style['bottom']}>
                   <div className={style['bottom-title']}>내용</div>
-                  <div>
+                  <div className={style['bottom-info']}>
                     <span>서비스 출시, 리뉴얼 및 고도화진행</span>
+                    <span>xstate, recoil을 통한 전역 상태 관리</span>
+                    <span>storybook을 사용한 테스트 자동화 경험</span>
                   </div>
                 </div>
               </div>
             </SwiperSlide>
-            <SwiperSlide className={style['swiper-slide']}>2</SwiperSlide>
+            <SwiperSlide className={style['swiper-slide']}>
+              <div className={style['img-wrapper']}>
+                <img
+                  src="/images/png/gmp.jpg"
+                  alt="rela"
+                  onClick={() => {
+                    window.open('https://gmp.geniemusic.co.kr/');
+                  }}
+                />
+              </div>
+              <div className={style['contents']}>
+                <div className={style['top']}>
+                  <span
+                    className={style['top-title']}
+                    style={{ color: '#4775F6' }}
+                  >
+                    GMP
+                  </span>
+                  <span className={style['top-info']}>
+                    지니뮤직 파트너들을 위한 분석 사이트
+                  </span>
+                </div>
+                <div className={style['line']}></div>
+                <div className={style['bottom']}>
+                  <div className={style['bottom-title']}>내용</div>
+                  <div className={style['bottom-info']}>
+                    <span>
+                      사용자 경험을 고려한 아이디어 제안 및 서비스 출시,
+                      고도화진행
+                    </span>
+                    <span>Rechart를 사용한 데이터 시각화</span>
+                    <span>recoil을 통한 전역 상태 관리</span>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
           </Swiper>
         </section>
       </div>
